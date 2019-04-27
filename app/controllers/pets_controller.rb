a9f285feb3f88d443aa6d132bf5d063e657158cd
J. Singleton <jennifer.singleton@gmail.com>
2019-04-27 19:13:55 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -15,6 +15,7 @@ class PetsController < ApplicationController
     if !params[:owner][:name].empty?
       @pet.owner = Owner.create(params[:owner])
     end
+    binding.pry
 
     redirect to "pets/#{@pet.id}"
   end
