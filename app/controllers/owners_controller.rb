59fbae8fb919bd249a0775303efccedd6e99b1e6
J. Singleton <jennifer.singleton@gmail.com>
2019-04-27 18:38:32 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -33,7 +33,6 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do
-    binding.pry
     if !params[:owner].keys.include?("pets_ids")
       params[:owner]["pets_ids"] = []
     end
