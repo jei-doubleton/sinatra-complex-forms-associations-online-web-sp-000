c9fccbd720e517eef3b980964c7936acc64d4d9e
J. Singleton <jennifer.singleton@gmail.com>
2019-04-27 18:11:18 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -23,7 +23,7 @@ class OwnersController < ApplicationController
 
   get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
-    @pets = Owner.pets
+    @pets = @owner.pets
     erb :'/owners/edit'
   end
 
